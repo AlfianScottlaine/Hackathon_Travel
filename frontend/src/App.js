522971ca1d0254f376ec775e7cf48acf90c581da
Scottlaine <alfian.scottlaine@gmail.com>
2022-11-24 18:08:19 +0100

    add la base

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './assets/Header.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Header} className="Header" alt="Header" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
